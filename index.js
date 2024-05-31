a7b237307edb1063ab02e4c5f5c6a071a12a95ab
nishukum1211 <nishukum1211@gmail.com>
2024-05-31 21:23:52 +0530

    Initial commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.urlencoded());
+app.use(express.urlencoded());//added parser which is middleware
 app.use(express.static('assets'));
 
 
